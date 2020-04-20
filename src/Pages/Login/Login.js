d14fca54ad930939a985dbbdb697eaaf49be491f
ppoote <ppoote@gmail.com>
2020-04-20 19:52:50 +0900

    로그인 페이지 올림

---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Login.scss';
+
+
+// 리액트에서는 커스텀 태그인 컴포넌트를 만들 때
+//class 
+
+
+class Login extends React.Component {
+
+
+
+
+    render() {
+         return(
+             <div>로그인 페이지</div>
+         )
+    }
+
+}
+
+
+
+
+
+export default Login;
+
