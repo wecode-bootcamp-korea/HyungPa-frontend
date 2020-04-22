1c746f620d7247ef4714c4ff3e2a55cd525494af
junyeongCHOI <asxd153@gmail.com>
2020-04-22 20:56:02 +0900

    리뷰 디테일 페이지 아티클 부분 완성

--- src/Pages/ReviewDetail/ReviewComment/ReviewComment.js
+++ src/Pages/ReviewDetail/ReviewComment/ReviewComment.js
@@ -1,35 +1,48 @@
 import React, { Component } from "react";
+import CommentNoItem from "./CommentNoItem/CommentNoItem";
+import CommentList from "./CommentList/CommentList";
 import "./ReviewComment.scss";
 
 class ReviewComment extends Component {
-    constructor(props) {
-        super(props);
-    }
+  sumbitComment = (e) => {
+    e.preventDefault();
+  };
 
-    render() {
-        const { comment } = this.props;
-        return (
-            <div className='ReviewComment'>
-                <div className='commentInput'>
-                    <div className='commentText'>
-                        <span>답글 </span>
-                        <span className='countComment'>{comment.length}</span>
-                    </div>
-                    <form className='inputBox'>
-                        <input type='text' placeholder='답글을 남겨보세요' />
-                        <div className='inputIcons'>
-                            <i className='xi-emoticon-smiley-o' />
-                            <i className='xi-camera-o' />
-                        </div>
-                        <button type='submit'>등록하기</button>
-                    </form>
-                </div>
-                <div className='sort'>
-                    인기순 <i className='xi-caret-down-min' />
-                </div>
+  render() {
+    const { postUser, comment } = this.props;
+    return (
+      <div className="ReviewComment">
+        <div className="commentInput">
+          <div className="commentText">
+            <span>답글 </span>
+            <span className="countComment">{comment.length}</span>
+          </div>
+          <form className="inputBox">
+            <input type="text" placeholder="답글을 남겨보세요 !" />
+            <div className="inputIcons">
+              <i className="xi-emoticon-smiley-o" />
+              <i className="xi-camera-o" />
             </div>
-        );
-    }
+            <button type="submit" onClick={this.sumbitComment}>
+              등록하기
+            </button>
+          </form>
+        </div>
+        <div className="sortContainer">
+          <div className="sort">
+            인기순 <i className="xi-caret-down-min" />
+          </div>
+        </div>
+        <div className="commentListBox">
+          {comment.length === 0 ? (
+            <CommentNoItem postUser={postUser} />
+          ) : (
+            comment.map((commentItem) => <CommentList comment={commentItem} />)
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ReviewComment;
--- src/Pages/ReviewDetail/ReviewDetail.js
+++ src/Pages/ReviewDetail/ReviewDetail.js
@@ -2,33 +2,59 @@ import React, { Component } from "react";
 import Aheader from "./Aheader/Aheader";
 import Abody from "./Abody/Abody";
 import ReviewComment from "./ReviewComment/ReviewComment";
+import MoreReview from "./MoreReview/MoreReview";
 import "./ReviewDetail.scss";
 
 class ReviewDetail extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            comment:[1,2,3,4,5],
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      postUser: "zong",
+      comment: [
+        { name: "jun", text: "testestesty" },
+        { name: "one", text: "안녕하세요 원철입니다." },
+        { name: "oasdasdne", text: "dsadasd" },
+      ],
+      moreReviews: [
+        {
+          brand: "이니스프리",
+          product: "비자 트러블 클렌징젤",
+          rate: "5.0",
+          date: "2020.04.18",
+        },
+        {
+          brand: "페리페라",
+          product: "맑게 물든 벨벳 치크 [10호 겉바속촉 말린진저 ]",
+          rate: "4.5",
+          date: "2020.04.12",
+        },
+      ],
+    };
+  }
 
-    render() {
-        const { comment } = this.state;
-        return (
-            <div className='ReviewDetail'>
-                <article>
-                    <div className='articleWrap'>
-                        <Aheader />
-                        <Abody />
-                    </div>
-                    <ReviewComment comment={comment}/>
-                </article>
-                <div className='rightProfile'>
-                    <div className='profileWrap'>test</div>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    const { postUser, comment, moreReviews } = this.state;
+    return (
+      <div className="ReviewDetail">
+        <article>
+          <div className="articleWrap">
+            <Aheader />
+            <Abody />
+          </div>
+          <ReviewComment postUser={postUser} comment={comment} />
+          <div className="moreReviewText">
+            <span>{postUser}</span>님의 다른 파워리뷰
+          </div>
+          {moreReviews.map((moreReview) => (
+            <MoreReview moreReview={moreReview} />
+          ))}
+        </article>
+        <div className="rightProfile">
+          <div className="profileWrap">test</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ReviewDetail;
